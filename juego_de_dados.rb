951ea2ee95d20cdfadc4cba14104f27cc6784af2
Ataru Moroboshi <ataru@server1.qaligo.com>
2013-05-04 12:55:35 -0700

    lanzar los dado

--- juego_de_dados.rb
+++ juego_de_dados.rb
@@ -1,1 +1,11 @@
-puts "Bienvenido en los juegos de los dado"
+puts "=> Bienvenido en los juegos de los dado"
+puts "=> Hola, como te llamas?"
+nombre = gets.chomp
+puts "=> Hola " + nombre + " vamos a jugar?"
+# el jugador lanza su propio dado
+a = rand(6) + 1
+puts "=> Hola #{nombre} hiciste #{a} con tu dado!"
+# los computadora lanza su proprio dado
+b = rand(6) + 1
+puts "=> la computafora hizo #{b}!"
+
