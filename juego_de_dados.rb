59d6b80b7c08c9437961559651013a57d3afd9b2
Ataru Moroboshi <ataru@server1.qaligo.com>
2013-05-06 12:54:43 -0700

    utilizando los arreglos

--- juego_de_dados.rb
+++ juego_de_dados.rb
@@ -1,25 +1,46 @@
 puts "=> Bienvenido en los juegos de los dado"
 puts "=> Hola, como te llamas?"
 nombre = gets.chomp
-puts "=> Hola " + nombre + " vamos a jugar?"
+puts "=> Hola " + nombre + " vamos a jugari con dos dados?"
+
+dado_1 = rand(6) + 1
+dado_2 = rand(6) + 1
+total = dado_1 + dado_2
+
+resultado_jugador = [ dado_1 , dado_2, total]
+
 # el jugador lanza su propio dado
-a = rand(6) + 1
-puts "=> Hola #{nombre} hiciste #{a} con tu dado!"
+
+puts "=> Hola #{nombre} el primer dado tiene #{resultado_jugador[0]} el segundo #{resultado_jugador[1]} y el total es #{resultado_jugador[2]}!"
+
+dado_11 = rand(6) + 1
+dado_22 = rand(6) + 1
+total2 = dado_11 + dado_22
+
+resultado_jugadoris = [ dado_11 , dado_22, total2]
+
+# la computadora lanza su propio dado
+
+puts "=> Hola Yo nel mi primer dado tengo #{resultado_jugadoris[0]} en el segundo tengo #{resultado_jugadoris[1]} y el total es #{resultado_jugadoris[2]}!"
+
+
+=begin
 # los computadora lanza su proprio dado
 b = rand(6) + 1
 puts "=> la computafora hizo #{b}!"
+=end
 
-if a > b 
+if resultado_jugador[2] > resultado_jugadoris[2]
 
   puts "=> Hei #{nombre} ganaste! Tuviste suerte!"
 
-elsif a < b 
+elsif resultado_jugador[2] < resultado_jugadoris[2]
 
-  puts "=> Hei #{nombre} eres un loser! Yo gane!"
+   puts "=> Hei #{nombre} eres un loser! Yo gane!"
 
 else
 
-  a == b
+  resultado_jugador[2] == resultado_jugadoris[2]
 
   puts "=> Empate! Amigos como antes!"
 
